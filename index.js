3fd26777c3c5f9ab62f06ddfbf6812b68b6e6b61
shahbazaamir <shahbaz07dbit@gmail.com>
2019-08-06 22:20:18 +0530

    firebase added

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+
+
+
+
 import Vue from 'vue'
+import firebase from 'firebase'
+
+var config = {
+  apiKey: "AIzaSyAi_yuJciPXLFr_PYPeU3eTvtXf8jbJ8zw",
+  authDomain: "vue-demo-537e6.firebaseapp.com",
+  databaseURL: "https://vue-demo-537e6.firebaseio.com"
+}
+firebase.initializeApp(config);
 
 new Vue({
   el: '#app',
