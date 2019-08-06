4fc367353bd034ddf609d2aa613ed457e15c5ab1
shahbazaamir <shahbaz07dbit@gmail.com>
2019-08-06 21:35:58 +0530

    vue js

--- index.js
+++ index.js
@@ -2,5 +2,18 @@ import Vue from 'vue'
 
 new Vue({
   el: '#app',
-  template: '<h1>hello Vue</h1>'
+  template: `<div>
+  <h1>hello vvue </h1>
+   <p> {{message}} bye changed </p> 
+   <user></user>
+   </div>`,
+  data : {
+    message : 'hi'
+  } ,
+
 })
+
+
+Vue.component('user',{
+   template : '<div><h1> User component</h1></div>'
+});
